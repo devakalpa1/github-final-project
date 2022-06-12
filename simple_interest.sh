86ead072c520973d67979b6972f9ec4847ea39b9
devakalpa1 <66040982+devakalpa1@users.noreply.github.com>
2022-06-12 11:55:59 -0500

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Geoffrey Stephens
    # Additional Authors:
-   # <your GitHub username>
+   # devakalpa1
 
    # Input:
    # p, principal amount
